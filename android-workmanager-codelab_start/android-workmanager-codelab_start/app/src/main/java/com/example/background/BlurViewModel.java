b9b08d2610057c0b998fec9c1ada5f4c9c15d6ef
ginowine <emailosahon@yahoo.com>
2020-02-20 10:40:33 +0100

    Initial commit

---
+++ android-workmanager-codelab_start/android-workmanager-codelab_start/app/src/main/java/com/example/background/BlurViewModel.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.background;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkContinuation;
+import androidx.work.WorkManager;
+
+import android.app.Application;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import com.example.background.workers.BlurWorker;
+import com.example.background.workers.CleanupWorker;
+import com.example.background.workers.SaveImageToFileWorker;
+
+import static com.example.background.Constants.KEY_IMAGE_URI;
+
+public class BlurViewModel extends AndroidViewModel {
+
+    private Uri mImageUri;
+    private WorkManager mWorkManager;
+
+    public BlurViewModel(@NonNull Application application) {
+        super(application);
+        mWorkManager = WorkManager.getInstance(application);
+    }
+
+    /**
+     * Create the WorkRequest to apply the blur and save the resulting image
+     * @param blurLevel The amount to blur the image
+     */
+    void applyBlur(int blurLevel) {
+
+        // Add WorkRequest to Cleanup temporary images
+        WorkContinuation continuation =
+                mWorkManager.beginWith(OneTimeWorkRequest.from(CleanupWorker.class));
+
+        // Add WorkRequest to blur the image
+        OneTimeWorkRequest blurRequest = new OneTimeWorkRequest.Builder(BlurWorker.class)
+                .setInputData(createInputDataForUri())
+                .build();
+        continuation = continuation.then(blurRequest);
+
+
+        // Add WorkRequest to save the image to the filesystem
+        OneTimeWorkRequest save =
+                new OneTimeWorkRequest.Builder(SaveImageToFileWorker.class)
+                        .build();
+        continuation = continuation.then(save);
+
+        // Actually start the work
+        continuation.enqueue();
+    }
+
+    /**
+     * Creates the input data bundle which includes the Uri to operate on
+     * @return Data which contains the Image Uri as a String
+     */
+    private Data createInputDataForUri(){
+        Data.Builder builder = new Data.Builder();
+        if (mImageUri != null){
+            builder.putString(KEY_IMAGE_URI, mImageUri.toString());
+        }
+        return builder.build();
+    }
+
+    private Uri uriOrNull(String uriString) {
+        if (!TextUtils.isEmpty(uriString)) {
+            return Uri.parse(uriString);
+        }
+        return null;
+    }
+
+    /**
+     * Setters
+     */
+    void setImageUri(String uri) {
+        mImageUri = uriOrNull(uri);
+    }
+
+    /**
+     * Getters
+     */
+    Uri getImageUri() {
+        return mImageUri;
+    }
+
+}
